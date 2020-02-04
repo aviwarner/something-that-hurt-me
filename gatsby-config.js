cc9f41d805e0bd73849c12d07b2d12857e1be50d
Avi Warner <aviwarner@gmail.com>
2020-02-04 14:48:51 -0800

    feat: removed most Gatsby build refs

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,12 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Something that hurts me`,
+    description: `Many night before bed, after books are read and teeth are brushed, my son (3) will pause, look a bit pensive, and say, "Something that hurt me...", following up with something he doesn't like.
+
+    It's a classic bed-time stalling maneuver. The general counter is that I ask him for something else that makes him feel good.
+    
+    The answers are generally charming though, and I'm building a site to celebrate this fleeting moment in his young life.`,
+    author: `@aviwarner`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -18,13 +22,13 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `something-that-hurt-me`,
+        short_name: `sthm`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#42240c`,
+        theme_color: `#42240c`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/sad_favicon.png`, // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
--- src/components/header.js
+++ src/components/header.js
@@ -5,7 +5,7 @@ import React from "react"
 const Header = ({ siteTitle }) => (
   <header
     style={{
-      background: `rebeccapurple`,
+      background: `#42240c`,
       marginBottom: `1.45rem`,
     }}
   >
